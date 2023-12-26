c02459d86d2ac564795668d044197e4804fa9869
제미소/정보영 <154112385+boyoung9020@users.noreply.github.com>
2023-12-26 16:28:33 +0900

    네이버뉴스 전체 카테고리 크롤링

--- crawling.py
+++ crawling.py
@@ -47,62 +47,88 @@ def clean_title(title):
 # 세계       104       100페이지
 # IT/과학    105       80페이지
 
+category = {100: 180, 101: 400, 102: 570, 103: 80, 104: 100, 105: 80}
 
-def get_naver_news_content(start_page=1, end_page=80):
-    base_url = "https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=105#&date=%2000:00:00&page={}"
+def get_naver_news_content(start_page=1):
+    all_contents = []  # 각 카테고리의 결과를 모아서 반환할 리스트
 
-    options = Options()
-    # options.add_argument("--headless")  # 백그라운드 모드로 실행 (창이 뜨지 않음)
-    driver = webdriver.Chrome(options=options)
+    for key, value in category.items():
+        base_url1 = f"https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1={key}#&date=%2000:00:00&"
+        base_url2 = "page={}"
 
-    unique_titles = set()
-    contents = []
+        options = Options()
+        options.add_argument("--headless")  # 백그라운드 모드로 실행 (창이 뜨지 않음)
+        driver = webdriver.Chrome(options=options)
 
-    for page in tqdm(range(start_page, end_page + 1), desc="Progress", unit="page"):
-        url = base_url.format(page)
-        driver.get(url)
+        unique_titles = set()
+        contents = []
 
-        # 페이지가 로딩될 때까지 기다릴 수 있는 코드 추가 (예: 10초)
-        driver.implicitly_wait(10)
-
-        # 현재 페이지의 내용 가져오기
-        soup = BeautifulSoup(driver.page_source, "html.parser")
-
-        for content in soup.select(".type06 li"):
-            title = content.select_one("dt:not(.photo) a").text.strip()
-
-            # 중복된 title은 무시
-            if title not in unique_titles:
-                unique_titles.add(title)
-
-                cleaned_title = clean_title(title)
-
-                # split_title 함수 사용
-                # first_quarter, second_quarter, third_quarter, fourth_quarter = split_title(title)
-
-                item = {
-                    # "title_first_quarter": first_quarter,
-                    # "title_second_quarter": second_quarter,
-                    # "title_third_quarter": third_quarter,
-                    # "title_fourth_quarter": fourth_quarter,
-                    "title": cleaned_title
-                }
-
-                contents.append(item)
-
-    driver.quit()  # 작업이 끝나면 드라이버 종료
-
-    return contents
+        category_name = ""
+        if key == 100:
+            category_name = '정치'
+        elif key == 101:
+            category_name = '경제'
+        elif key == 102:
+            category_name = '사회'
+        elif key == 103:
+            category_name = '생활/문화'
+        elif key == 104:
+            category_name = '세계'
+        elif key == 105:
+            category_name = 'IT/과학'
+        else :
+            category_name = "???"    
 
 
-def save_to_text(contents, filename="output.txt"):
+        for page in tqdm(range(start_page, value + 1), desc=category_name, unit="page"):
+            base_url = base_url1+base_url2
+            url = base_url.format(page)
+            driver.get(url)
+
+            # 페이지가 로딩될 때까지 기다릴 수 있는 코드 추가 (예: 10초)
+            driver.implicitly_wait(10)
+
+            # 현재 페이지의 내용 가져오기
+            soup = BeautifulSoup(driver.page_source, "html.parser")
+
+            for content in soup.select(".type06 li"):
+                title = content.select_one("dt:not(.photo) a").text.strip()
+
+                # 중복된 title은 무시
+                if title not in unique_titles:
+                    unique_titles.add(title)
+
+                    cleaned_title = clean_title(title)
+
+                    # split_title 함수 사용
+                    # first_quarter, second_quarter, third_quarter, fourth_quarter = split_title(title)
+
+                    item = {
+                        # "title_first_quarter": first_quarter,
+                        # "title_second_quarter": second_quarter,
+                        # "title_third_quarter": third_quarter,
+                        # "title_fourth_quarter": fourth_quarter,
+                        "title": cleaned_title
+                    }
+
+                    contents.append(item)
+
+        driver.quit()  # 작업이 끝나면 드라이버 종료
+
+        all_contents.extend(contents)  # 현재 카테고리의 결과를 전체 결과에 추가
+
+    return all_contents  # 최종적으로 전체 결과 반환
+
+
+
+def save_to_text(all_contents, filename="output.txt"):
     with open(filename, "w", encoding="utf-8") as file:
-        for content in contents:
+        for all_contents in all_contents:
             # file.write(f"{content['title_first_quarter']}\n")
             # file.write(f"{content['title_second_quarter']}\n")
             # file.write(f"{content['title_third_quarter']}\n")
             # file.write(f"{content['title_fourth_quarter']}\n")
-            file.write(f"{content['title']}\n")
+            file.write(f"{all_contents['title']}\n")
 
 def open_notepad(filename="output.txt"):
     subprocess.run(["notepad.exe", filename], check=True)
