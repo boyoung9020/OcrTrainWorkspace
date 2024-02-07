4cd372e1d69973304bef6675294d6daac1b56b71
제미소/정보영 <154112385+boyoung9020@users.noreply.github.com>
2024-02-07 15:30:14 +0900

    txt2img.py  v2.0 옵션 추가, 회전추가

--- txt2img.py
+++ txt2img.py
@@ -5,19 +5,20 @@ from tqdm import tqdm
 import random
 import warnings
 from concurrent.futures import ProcessPoolExecutor
+import argparse
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-
 def create_text_images(args):
+    line, font_directory, save_output_path, counter, tilt = args  
+    # print(tilt)
     try:
-        line, font_directory, save_output_path, counter = args
         target_height_ratio = 0.98
         height = 120
         base_font_size = 80
         font_sizes = list(range(base_font_size - 10, base_font_size + 9))  
         font_size = random.choice(font_sizes)
-        print(f"fs={font_size} {line}")
+        # print(f"fs={font_size} {line}")
 
         image_paths = []
         labels = []
@@ -45,28 +46,34 @@ def create_text_images(args):
             draw.text((current_x_position, y_position), char, font=font, fill="black")
             current_x_position += char_width + character_spacing
     
-        img = img.crop((50, 0, width , height))
+        # img = img.crop((50, 0, width , height))
 
-        # 입력 텍스트 길이가 5 이상일 때 회전 각도를 최대 2도로 제한
-        if len(filtered_words) == 1:
-            rotation_angle = 0
-        # 입력 텍스트 길이가 5 이상인 경우 회전 각도를 최대 2도로 제한
-        elif len(filtered_words) >= 5:
-            rotation_angle = random.randint(-2, 2)
-        else:
-            rotation_angle = random.randint(-8, 8)  # 5 미만인 경우 -10도부터 10도 사이에서 랜덤하게 회전
+        rotation_angle = 0
+        if tilt:
+            if len(filtered_words) == 1:
+                rotation_angle = random.uniform(-0.3, 0.3)
+            elif len(filtered_words) < 30:
+                if font_size > 80:
+                    rotation_angle = random.randint(-2, 2)
+                else:
+                    rotation_angle = random.randint(-1, 1)
+            elif font_size >= 88:
+                rotation_angle = random.uniform(-0.3, 0.3)
+            else:
+                rotation_angle = random.randint(-8, 8)
 
-        # 이미지를 회전
-        img = img.rotate(rotation_angle, expand=True, fillcolor=(238, 238, 238))
+            img = img.rotate(rotation_angle, resample=Image.BICUBIC, expand=True, fillcolor=(238, 238, 238))  # resample=Image.BICUBIC을 추가하여 이미지를 회전할 때 Bicubic 보간
 
         # 회전 후 높이가 120이 되도록 자르기
-        img_height = img.size[1]
-        if img_height > height:
-            upper = (img_height - height) // 2
-            lower = upper + height
-            img = img.crop((0, upper, width, lower))
+            img_height = img.size[1]
+            if img_height > height:
+                upper = (img_height - height) // 2
+                lower = upper + height
+                img = img.crop((0, upper, width, lower))
 
-        img = img.crop((0, 0, width -48  , height))
+            img = img.crop((50, 0, width - 7, height))
+
+        img = img.crop((50, 0, width, height))
 
         output_path = os.path.join(save_output_path, f"images\image_{counter:04d}.jpg")
         img.save(output_path)
@@ -78,6 +85,7 @@ def create_text_images(args):
     except Exception as e:
         print(f"Exception: {e}")
         raise
+
 
 def delete_files_in_directory(directory):
     print(f"{directory} 삭제중...")
@@ -91,6 +99,7 @@ def delete_files_in_directory(directory):
                 print(f"파일 삭제 오류 {file_path}: {e}")
 
 def count_images_in_directory(output_path):
+    print(f"{output_path} count 중.. ")
     count = 0
     images_folder_path = os.path.join(output_path, 'images')
     for filename in os.listdir(images_folder_path):
@@ -103,14 +112,25 @@ def split_and_save(text_file_path, split_text_file_path):
     with open(text_file_path, 'r', encoding='utf-8') as f:
         content = f.read()
 
-    sentences = content.split()
+    sentences = content.split()  
+    sentences = list(filter(None, sentences))  # 빈 문자열을 제거
+
 
     with open(split_text_file_path, 'w', encoding='utf-8') as f:
         for sentence in tqdm(sentences, desc='Splitting', unit='sentence'):
             f.write(sentence + '\n')
 
+
 ########### training dataset 생성
-def create_training_dataset():
+def create_training_dataset(args):
+    tilt = args.tilt
+    training_text_file_path = args.training_text_file_path
+    training_split_text_file_path = args.training_split_text_file_path
+    training_output_path = args.training_output_path
+    font_directory = args.font_directory
+
+    split_and_save(training_text_file_path, training_split_text_file_path)
+    
     print(f"{training_split_text_file_path} 읽는중.....")
     with open(training_split_text_file_path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
@@ -119,16 +139,23 @@ def create_training_dataset():
     with open(training_split_text_file_path, 'r', encoding='utf-8') as file, open(os.path.join(training_output_path, 'gt.txt'), 'w', encoding='utf-8') as gt_file:
         
         with ProcessPoolExecutor() as executor:
-            results = list(tqdm(executor.map(create_text_images, [(line, font_directory, training_output_path, counter) for counter, line in enumerate(lines, start=1)], chunksize=50), total=len(lines), desc='Creating Training Data', unit='image'))
+            results = list(tqdm(executor.map(create_text_images, [(line, font_directory, training_output_path, counter, tilt) for counter, line in enumerate(lines, start=1)], chunksize=50), total=len(lines), desc='Creating Training Data', unit='image'))
 
             for result in results:
                 image_paths_str = ', '.join(result[0])  # 쉼표로 구분된 문자열로 변환
                 gt_file.write(f"{image_paths_str}\t{result[1]}")
     print('-' * 80)
-            
+
 
 ########### validation dataset 생성             
-def create_validation_dataset():
+def create_validation_dataset(args):
+    tilt = args.tilt
+    validation_text_file_path = args.training_text_file_path
+    validation_split_text_file_path = args.validation_split_text_file_path
+    validation_output_path = args.validation_output_path
+    font_directory = args.font_directory
+    
+    split_and_save(validation_text_file_path, validation_split_text_file_path)
     print(f"{validation_split_text_file_path} 읽는중.....")
     with open(validation_split_text_file_path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
@@ -136,11 +163,10 @@ def create_validation_dataset():
     delete_files_in_directory(validation_output_path)
         
     with open(validation_split_text_file_path, 'r', encoding='utf-8') as file, open(os.path.join(validation_output_path, 'gt.txt'), 'w', encoding='utf-8') as gt_file:
-        # total_images = count_images_in_directory(training_output_path)
-        # validation_images_count = int(total_images*0.01)
+        total_images = count_images_in_directory(args.training_output_path)
+        validation_images_count = int(total_images*0.01)
         with ProcessPoolExecutor() as executor:
-            results = list(tqdm(executor.map(create_text_images, ((line, font_directory, validation_output_path, counter) for counter, line in enumerate(lines, start=1) if counter <= 20), chunksize=50), total=20, desc='Creating Training Data', unit='image'))
-
+            results = list(tqdm(executor.map(create_text_images, [(line, font_directory, validation_output_path, counter, tilt) for counter, line in enumerate(lines, start=1) if counter <= validation_images_count], chunksize=50), total=validation_images_count, desc='Creating Training Data', unit='image'))
             for result in results:
                 image_paths_str = ', '.join(result[0])  # 쉼표로 구분된 문자열로 변환
                 gt_file.write(f"{image_paths_str}\t{result[1]}")
@@ -148,64 +174,171 @@ def create_validation_dataset():
                          
 
 ########### test dataset 생성             
-def create_test_dataset():
+def create_test_dataset(args):
+    tilt = args.tilt
+    new_test_text_file_path = args.new_test_text_file_path
+    test_output_path = args.test_output_path
+    font_directory = args.font_directory
+    
     print(f"{new_test_text_file_path} 읽는중.....")
     with open(new_test_text_file_path, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
+        lines = [line.strip() for line in f if line.strip()] 
     delete_files_in_directory(test_output_path)
     
     with open(new_test_text_file_path, 'r', encoding='utf-8') as file, open(os.path.join(test_output_path, 'gt.txt'), 'w', encoding='utf-8') as gt_file:
-        filtered_lines = [line for line in lines if len(line) <=30]
+        filtered_lines = [line.strip() for line in lines if len(line.strip()) <= 30]  # 앞뒤 공백 제거 후 길이가 30 이하인 줄만 선택
         with ProcessPoolExecutor() as executor:
-            results = list(tqdm(executor.map(create_text_images, [(filtered_lines, font_directory, test_output_path, counter) for counter, filtered_lines in enumerate(filtered_lines, start=1) ], chunksize=50), total=len(filtered_lines), desc='Creating Training Data', unit='image'))
+            results = list(tqdm(executor.map(create_text_images, [(filtered_lines, font_directory, test_output_path, counter, tilt) for counter, filtered_lines in enumerate(filtered_lines, start=1) ], chunksize=50), total=len(filtered_lines), desc='Creating Training Data', unit='image'))
             
             for result in results:
                 image_paths_str = ', '.join(result[0])  # 쉼표로 구분된 문자열로 변환
-                gt_file.write(f"{image_paths_str}\t{result[1]}")
+                gt_file.write(f"{image_paths_str}\t{result[1]}\n")  # 각 줄 끝에 개행문자('\n') 추가
     print('-' * 80)
 
-                
-script_directory = os.path.dirname(__file__)
 
-training_text_file_path = os.path.join(script_directory, "crawling/headlinedatatxt/training_all_category.txt")
-training_split_text_file_path = os.path.join(script_directory, "crawling/headlinedatatxt/training_all_category_split.txt")
-validation_text_file_path = os.path.join(script_directory, "crawling/headlinedatatxt/validation_all_category.txt")
-validation_split_text_file_path = os.path.join(script_directory, "crawling/headlinedatatxt/validation_all_category_split.txt")
-training_output_path = os.path.join(script_directory, "step2/training/kordata/")
-test_output_path = os.path.join(script_directory, "step2/test/kordata/")
-validation_output_path = os.path.join(script_directory, "step2/validation/kordata/")
-test_text_file_path = os.path.join(script_directory, "crawling/headlinedatatxt/test_all_category.txt")
-new_test_text_file_path = os.path.join(script_directory, "crawling/headlinedatatxt/new_test_all_category.txt")
-test1_text_file_path = os.path.join(script_directory, "crawling/headlinedatatxt/test1.txt")
-words_text_file_path = os.path.join(script_directory, "crawling/headlinedatatxt/words.txt")
-test_split_text_file_path = os.path.join(script_directory, "crawling/headlinedatatxt/test_all_category_split.txt")
-chinese_font_path = os.path.join(script_directory, "font/SourceHanSansK-Regular.otf")
-korean_font_path = os.path.join(script_directory, "font/malgunbd.ttf")
-font_path = os.path.join(script_directory, "font/KoreanGD17R.ttf") 
-font_directory = os.path.join(script_directory, "font")
-
-
-images_folder_path = os.path.join(training_output_path, 'images')
-if not os.path.exists(images_folder_path):
-    os.makedirs(images_folder_path)
-
-
-images_folder_path = os.path.join(validation_output_path, 'images')
-if not os.path.exists(images_folder_path):
-    os.makedirs(images_folder_path)
-
-images_folder_path = os.path.join(test_output_path, 'images')
-if not os.path.exists(images_folder_path):
-    os.makedirs(images_folder_path)
 
 def get_all_fonts_in_directory(font_directory):
     font_paths = [os.path.join(font_directory, file) for file in os.listdir(font_directory) if file.lower().endswith(('.ttf', '.otf'))]
     return font_paths
 
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    script_directory = os.path.dirname(__file__)
+    
+    parser.add_argument(
+        '-t',
+        '--tilt',
+        action='store_true',
+        help='tilt value of text'
+    )
+    parser.add_argument(
+        '-td',
+        '--training_data',
+        action='store_true',
+        help='create training dataset'
+    )
+    parser.add_argument(
+        '-vd',
+        '--validation_data',
+        action='store_true',
+        help='create validation dataset'
+    )
+    parser.add_argument(
+        '-tdvd',
+        '--training_validation_data',
+        action='store_true',
+        help='create test dataset'
+    )
+    parser.add_argument(
+        '-ttd',
+        '--test_data',
+        action='store_true',
+        help='create test dataset'
+    )
+    parser.add_argument(
+        '--training_text_file_path',
+        default=os.path.join(script_directory, "crawling/headlinedatatxt/training_all_category.txt"),
+        help='path to training text file'
+    )
+    parser.add_argument(
+        '--training_split_text_file_path',
+        default=os.path.join(script_directory, "crawling/headlinedatatxt/training_all_category_split.txt"),
+        help='path to training split text file'
+    )
+    parser.add_argument(
+        '--validation_text_file_path',
+        default=os.path.join(script_directory, "crawling/headlinedatatxt/validation_all_category.txt"),
+        help='path to validation text file'
+    )
+    parser.add_argument(
+        '--validation_split_text_file_path',
+        default=os.path.join(script_directory, "crawling/headlinedatatxt/validation_all_category_split.txt"),
+        help='path to validation split text file'
+    )
+    parser.add_argument(
+        '--training_output_path',
+        default=os.path.join(script_directory, "step2/training/kordata/"),
+        help='path to training output directory'
+    )
+    parser.add_argument(
+        '--test_output_path',
+        default=os.path.join(script_directory, "step2/test/kordata/"),
+        help='path to test output directory'
+    )
+    parser.add_argument(
+        '--validation_output_path',
+        default=os.path.join(script_directory, "step2/validation/kordata/"),
+        help='path to validation output directory'
+    )
+    parser.add_argument(
+        '--test_text_file_path',
+        default=os.path.join(script_directory, "crawling/headlinedatatxt/test_all_category.txt"),
+        help='path to test text file'
+    )
+    parser.add_argument(
+        '--new_test_text_file_path',
+        default=os.path.join(script_directory, "crawling/headlinedatatxt/new_test_all_category.txt"),
+        help='path to new test text file'
+    )
+    parser.add_argument(
+        '--test1_text_file_path',
+        default=os.path.join(script_directory, "crawling/headlinedatatxt/test1.txt"),
+        help='path to test1 text file'
+    )
+    parser.add_argument(
+        '--words_text_file_path',
+        default=os.path.join(script_directory, "crawling/headlinedatatxt/words.txt"),
+        help='path to words text file'
+    )
+    parser.add_argument(
+        '--test_split_text_file_path',
+        default=os.path.join(script_directory, "crawling/headlinedatatxt/test_all_category_split.txt"),
+        help='path to test split text file'
+    )
+    parser.add_argument(
+        '--chinese_font_path',
+        default=os.path.join(script_directory, "font/SourceHanSansK-Regular.otf"),
+        help='path to chinese font file'
+    )
+    parser.add_argument(
+        '--korean_font_path',
+        default=os.path.join(script_directory, "font/malgunbd.ttf"),
+        help='path to korean font file'
+    )
+    parser.add_argument(
+        '--font_path',
+        default=os.path.join(script_directory, "font/KoreanGD17R.ttf"),
+        help='path to font file'
+    )
+    parser.add_argument(
+        '--font_directory',
+        default=os.path.join(script_directory, "font"),
+        help='path to font directory'
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+
+    for output_path in [args.training_output_path, args.validation_output_path, args.test_output_path]:
+        args.images_folder_path = os.path.join(output_path, 'images')
+        if not os.path.exists(args.images_folder_path):
+            os.makedirs(args.images_folder_path)
+            
+    if args.training_data:
+        create_training_dataset(args)
+    elif args.validation_data:
+        create_validation_dataset(args)
+    elif args.test_data:
+        create_test_dataset(args)
+    elif args.training_validation_data:
+        create_training_dataset(args)
+        create_validation_dataset(args)
+    else:
+        raise ValueError("Invalid dataset type. Please specify either training, validation, or test.")
+
 if __name__ == "__main__":
-    # split_and_save(training_text_file_path, training_split_text_file_path)
-    # split_and_save(validation_text_file_path, validation_split_text_file_path)
     # split_and_save(test_text_file_path, test_split_text_file_path)
-    # create_training_dataset()
-    create_validation_dataset()
-    # create_test_dataset()
+    main()
